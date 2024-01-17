f6962c941673e1f1feb7b2e64a1c1ad40ac448be
Gedyael Lucas <lucasgedyael@gmail.com>
2024-01-17 00:12:19 -0300

    classes genericas

--- classegenerica.kt
+++ classegenerica.kt
@@ -1,18 +1,18 @@
-class Mutablepilha<E>(vararg intens : E) {
+class Mutablepilha<E>(vararg intens : E) {                      //1 criando classe com paramentos genericos "E", fazendo com o tipo dele seja qualquer coisa
 
-    private val elementos = intens.toMutableList()
+    private val elementos = intens.toMutableList()              //2 propriedade criada convertando para "toMutablelis()
 
-    fun push(elemento: E) = elementos.add(elemento)
+    fun push(elemento: E) = elementos.add(elemento)              //3- adicionando intem lista
 
-    fun peek(): E = elementos.last()
+    fun peek(): E = elementos.last()                             //4-  pega o ultimo elemento sem remoção
 
-    fun pop(): E = elementos.removeAt(elementos.size - 1)
+    fun pop(): E = elementos.removeAt(elementos.size - 1)  //5- removendo da lista
 
-    fun isEmpty() = elementos.isEmpty()
+    fun isEmpty() = elementos.isEmpty()                          //4- verificar se a lista ta vazia
 
-    fun size() = elementos.size
+    fun size() = elementos.size                                  //5- verificar o tamanho da lista
 
-    override fun toString() = "Mutablepilha(${elementos.joinToString()})"
+    override fun toString() = "Mutablepilha(${elementos.joinToString()})"  
 
 }
 
